f5ebfe1adfd53cbb8ee7b4e79490537b436534c0
aziz-chniti <azizchniti0@gmail.com>
2024-12-01 19:37:17 +0100

    new favicon

--- assets/js/main.js
+++ assets/js/main.js
@@ -52,66 +52,67 @@ const sendEmail = (e) => {
       "#contact-form",
       "ZbIWo9AEAdAxdcorC"
     )
-    .then(() => {
-      contactMessage.textContent = "Message sent successfully ✔";
+    .then(
+      () => {
+        contactMessage.textContent = "Message sent successfully ✔";
 
-      setTimeout(() => {
-        contactMessage.textContent = "";
-      }, 5000);
+        setTimeout(() => {
+          contactMessage.textContent = "";
+        }, 5000);
 
-      contactForm.reset();
-    },()=>{
-        contactMessage.textContent = 'Message not sent (service error) ❌'
-    });
+        contactForm.reset();
+      },
+      () => {
+        contactMessage.textContent = "Message not sent (service error) ❌";
+      }
+    );
 };
 
 contactForm.addEventListener("submit", sendEmail);
 
 /*=============== SHOW SCROLL UP ===============*/
-const scrollUp = ()=>{
-    const scrollUp = document.getElementById('scroll-up')
-    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
-                       : scrollUp.classList.remove('show-scroll');
-}
-window.addEventListener('scroll', scrollUp)
+const scrollUp = () => {
+  const scrollUp = document.getElementById("scroll-up");
+  this.scrollY >= 350
+    ? scrollUp.classList.add("show-scroll")
+    : scrollUp.classList.remove("show-scroll");
+};
+window.addEventListener("scroll", scrollUp);
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
-const sections = document.querySelectorAll('section[id]')
-
-
+const sections = document.querySelectorAll("section[id]");
 
 const scrollActive = () => {
-    const scrollDown = window.scrollY
+  const scrollDown = window.scrollY;
 
-    sections.forEach(current =>{
-        const sectionHeight = current.offsetHeight,
-              sectionTop = current.offsetTop - 58,
-              sectionId = current.getAttribute('id'),
-              sectionsClass = document.querySelector('.nav_menu a[href*=' + sectionId + ']')
-        
-        if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
-            sectionsClass.classList.add('active-link')
-        } else{
-            sectionsClass.classList.remove('active-link')
-        }
-    })
-}
-window.addEventListener('scroll', scrollActive)
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute("id"),
+      sectionsClass = document.querySelector(
+        ".nav_menu a[href*=" + sectionId + "]"
+      );
+
+    if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+      sectionsClass.classList.add("active-link");
+    } else {
+      sectionsClass.classList.remove("active-link");
+    }
+  });
+};
+window.addEventListener("scroll", scrollActive);
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
 
 const sr = ScrollReveal({
-    origin: 'top',
-    distance: '60px',
-    duration: 2500,
-    delay: 400,
-
-})
-sr.reveal(`.home_data, .experience , .skills, .contact_container`)
-sr.reveal(`.home_img`, {delay: 600})
-sr.reveal(`.home_scroll`, {delay: 800})
-sr.reveal(`.work_card, .services_card`, {interval: 100})
-sr.reveal(`.about_content`, {origin: 'left'})
-sr.reveal(`.about_img`, {origin: 'left'})
-
-
+  origin: "top",
+  distance: "60px",
+  duration: 2500,
+  delay: 400,
+});
+sr.reveal(`.home_data, .experience , .skills, .contact_container`);
+sr.reveal(`.home_img`, { delay: 600 });
+sr.reveal(`.home_scroll`, { delay: 800 });
+sr.reveal(`.work_card, .services_card`, { interval: 100 });
+sr.reveal(`.about_content`, { origin: "left" });
+sr.reveal(`.about_img`, { origin: "left" });
